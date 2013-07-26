8619102a4c1b1ddb3a8fa263a38483e2dcd00e63
Paul Ammann <meanmrclean@gmail.com>
2013-07-26 15:41:58 -0400

    added node compliler

---
+++ app.js
@@ -0,0 +1,42 @@
+//Module dependencies
+
+var express = require('express')
+	, stylus = require('stylus')
+	, nib = require('nib');
+var app =express();
+app.use(express.bodyParser());
+function compile(str,path){
+	return stylus(str)
+		.set('filename',path)
+		.use(nib());
+}
+app.set('views',__dirname+'/views');
+app.set('view engine','jade');
+app.use(stylus.middleware(
+	{
+	src: __dirname+'/public'
+	, compile:compile
+}));
+app.use(express.static(__dirname+'/public'));
+
+app.get('/',function(req,res){
+	res.render('index',
+		{title:"Hoos Working Where"}		)
+})
+app.get('/selectjob',function(req,res){
+	res.render('selectjob',
+		{title:"What's your plan"}		)
+})
+
+app.post('/selectjob/detailsubmission',function(req,res){
+	//var name = req.body.place;
+		//city = req.body.city,
+		//country= req.body.country;
+	console.log(" in "+req.body.data);
+	res.redirect("/usermap");
+});
+app.get('/usermap', function(req,res){
+	res.render('usermap',
+		{title:"Hoos Working Where"})
+})
+app.listen(3000);
